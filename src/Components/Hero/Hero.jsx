2c6c8b655b060db79629239641e102350cbc1ae9
Gops229 <gopikavenugopal229@gmail.com>
2023-10-17 19:45:34 +0530

    changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Typed from "react-typed";
 import Button from "../../Components/Button/Button";
-
+import "./Hero.css";
 const Hero = () => {
   return (
     <div className="text-white p-5">
@@ -9,15 +9,15 @@ const Hero = () => {
         <p className="text-deep-orange font-bold p-2">
           IEEE STUDENT BRANCH CEC
         </p>
-        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-3">
+        <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold md:py-3">
           Emergence 3.0
         </h1>
         <div className="flex justify-center items-center">
-          <p className="md:text-2xl sm:text-4xl text-xl font-bold py-4">
+          <p className="md:text-2xl sm:text-3xl text-xl font-bold py-4">
             Convergence of
           </p>
           <Typed
-            className="md:text-2xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+            className="md:text-2xl sm:text-3xl text-xl font-bold md:pl-4 pl-2"
             strings={["Innovation", "Learning", "Inspiration"]}
             typeSpeed={80}
             backSpeed={90}
@@ -30,7 +30,10 @@ const Hero = () => {
         {/* <button className="bg-deep-orange w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
           Register Now
         </button> */}
-        <Button children="Register Now" className="lg:mt-6 border-black hover:bg-black hover:border hover:border-[#EC431B] hover:text-[#EC431B] delay-75 transition-all ease-in-out" />
+        <Button
+          children="Register Now"
+          className="lg:mt-6 border-black hover:bg-black hover:border hover:border-[#EC431B] hover:text-[#EC431B] delay-75 transition-all ease-in-out"
+        />
       </div>
     </div>
   );

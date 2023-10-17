d0303daedf81aa98ddecbf171d48bdc08841d179
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2023-10-17 20:14:54 +0530

    Updated Navbar & AOS Duration

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react"; // Import useState
 
 const navigation = [
   { name: "Home", href: "#", current: true },
@@ -14,6 +15,12 @@ function classNames(...classes) {
 }
 
 export default function Example() {
+  const [currentItem, setCurrentItem] = useState(navigation[0]); // Initialize current item state
+
+  const handleClick = (item) => {
+    setCurrentItem(item);
+  };
+
   return (
     <Disclosure as="nav" className="fixed w-screen z-50 bg-[#0c0d0d]">
       {({ open }) => (
@@ -41,18 +48,19 @@ export default function Example() {
                 </div>
                 <div className="hidden pl-64 sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    <div className="flex items-center space-x-7"> {/* Center align navigation items */}
+                    <div className="flex items-center space-x-7">
                       {navigation.map((item) => (
                         <a
                           key={item.name}
                           href={item.href}
                           className={classNames(
-                            item.current
+                            item === currentItem
                               ? "bg-[#0c0d0d] text-[#E9441D]"
                               : "text-gray-300 hover:text-[#E9441D]",
                             "rounded-md px-3 py-2 text-sm font-medium"
                           )}
-                          aria-current={item.current ? "page" : undefined}
+                          onClick={() => handleClick(item)}
+                          aria-current={item === currentItem ? "page" : undefined}
                         >
                           {item.name}
                         </a>
@@ -63,7 +71,6 @@ export default function Example() {
               </div>
             </div>
           </div>
-
           <div className={`sm:hidden ${open ? 'block' : 'hidden'}`}>
             <div className="space-y-1 bg-[#0c0d0d] w-screen absolute px-2 pb-3 pt-2">
               {navigation.map((item) => (
@@ -72,12 +79,13 @@ export default function Example() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current
-                      ? " text-[#E9441D]"
+                    item === currentItem
+                      ? "text-[#E9441D]"
                       : "text-gray-300 hover-bg-gray-700 hover-text-white",
                     "block rounded-md px-3 py-2 text-base font-medium"
                   )}
-                  aria-current={item.current ? "page" : undefined}
+                  onClick={() => handleClick(item)}
+                  aria-current={item === currentItem ? "page" : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>

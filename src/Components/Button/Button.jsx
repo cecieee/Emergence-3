982d620d28131ea6c9127fe7bc15dd75b495bba4
Harikrishna A <Harikichus2018@gmail.com>
2023-10-17 16:37:33 +0530

    Changed some stylings

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <button className="bg-deep-orange w-[full] rounded-md font-semibold my-6 mx-auto p-3  text-black hover:bg-slate-400 duration-500">
+    <button className="bg-deep-orange w-[full] rounded-md font-semibold my-6 mx-auto p-3  text-black border-black hover:bg-black hover:border hover:border-[#EC431B] hover:text-[#EC431B] delay-75 transition-all ease-in-out">
       {props.children}
     </button>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,54 +1,12 @@
 "use client";
 
-import { DarkThemeToggle, Navbar } from "flowbite-react";
+import {  Navbar } from "flowbite-react";
 import Logo from "../../assets/Logo.png";
 import Button from "../Button/Button";
 export default function NavbarWithCTAButton() {
   return (
-    // <Navbar fluid rounded className="bg-[#000300]">
-    //   <Navbar.Brand href="https://flowbite-react.com">
-    //     <img src={Logo} className="mr-3 h-10 sm:h-9" alt="Flowbite React Logo" />
-    //     {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-    //       Flowbite React
-    //     </span> */}
-    //   </Navbar.Brand>
-    //   <div className="flex md:order-2">
-    //     <Button children="Register"></Button>
-    //     <Navbar.Toggle />
-    //   </div>
-    //   <Navbar.Collapse>
-    //     <Navbar.Link href="#" active className="text-semibold">
-    //       Home
-    //     </Navbar.Link>
-    //     <Navbar.Link href="#">About</Navbar.Link>
-    //     <Navbar.Link href="#">Events</Navbar.Link>
-    //     <Navbar.Link href="#">Tracks</Navbar.Link>
-    //     <Navbar.Link href="#">Contact</Navbar.Link>
-    //   </Navbar.Collapse>
-    // </Navbar>
     <>
-      
 
-    <Navbar fluid rounded className="bg-black text-white">
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
- 
     </>
   );
 }

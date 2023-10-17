40bc739fa6ff81fcb26ed5095c3994d57542a614
Harikrishna A <Harikichus2018@gmail.com>
2023-10-17 11:37:57 +0530

    Added responsive to footer

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -10,7 +10,7 @@ function Footer()
     const website_link="https://cecieee.org/"
     const linkedin_link="https://www.linkedin.com/company/cecieee/"
     const ticket_link=""
-    const Map_link="https://maps.app.goo.gl/tKuTktrDa6QfYT4dA"
+    const Map_link="https://maps.app.goo.gl/hcgkytZqFGLKTscR7"
     const jsxelement=(
         <>
 
@@ -19,20 +19,22 @@ function Footer()
                 <a href={Map_link} className="absolute w-1/4 h-full right-0 max-[950px]:hidden"></a>
                 <div className="p-10 z-10 max-[950px]:w-full">
                     <p className="text-5xl font-semibold max-[950px]:text-center">GET YOUR TICKET</p>
-                    <div className="flex items-center h-full max-[950px]:h-fit max-[950px]:py-10 max-[950px]:justify-center">
-                        <a href={ticket_link} className="bg-[#EC431B] h-12 w-[30vh] flex justify-center items-center border-black hover:bg-black hover:border hover:border-[#EC431B] hover:text-[#EC431B] delay-75 duration-150">Get your Ticket now</a>
+                    <div className="flex flex-col items-start justify-center h-full max-[950px]:h-fit max-[950px]:py-10 max-[950px]:justify-center">
+                        <p>Be part of the tech revolution at Emergence 3.0 - reserve your tickets today for a glimpse into tomorrow's world</p>
+                        <a href={ticket_link} className="bg-[#EC431B] h-12 w-[30vh] flex justify-center items-center border-black hover:bg-black hover:border hover:border-[#EC431B] hover:text-[#EC431B] delay-75 transition-all ease-in-out">Get your Ticket now</a>
+
                     </div>
                     <div className="min-[950px]:hidden relative">
                         {/* <div className="bg-gradient-to-b from-black to-transparent absolute bottom-0 w-full"></div> */}
-                        <a href={Map_link} className="w-full h-full block absolute"></a>
-                        <img src="./Group 5.png" alt="" className=" w-full "/>
+                        <a href={Map_link} className="w-full h-full block absolute z-10"></a>
+                        <img src="./Group 5.png" alt="" className=" w-full z-0"/>
                     </div>
                 </div>
             </div>
             <div className="  h-fit flex font-montserrat max-[950px]:flex-col-reverse">
                 <div className="basis-1/3 justify-center items-center flex flex-col mx-2">
                     <div className="w-fit h-fit">
-                        <img src="./black_logo.png" alt="" className="h-[13vh] min-[1570px]:h-[8vh]"/>
+                        <img src="./black_logo.png" alt="" className="h-[13vh] min-[1570px]:h-[8vh] max-[630px]:h-[6vh]"/>
                         <p className="self-start py-3 font-medium">All Rights Reserved.</p>
                     </div>
                 </div>
@@ -43,8 +45,8 @@ function Footer()
 
                 <div className="basis-1/3 items-end flex flex-col p-8 mr-5 gap-4 max-[950px]:items-center max-[950px]:pb-12">
                     <p className="font-extrabold text-4xl py-2">CONTACT US</p>
-                    <p>Swathy: 8378928747</p>
-                    <p className="pb-6">Akash: 1234567891</p>
+                    <p>Swathy: 7510669792</p>
+                    <p className="pb-6">Akash: 7994831729</p>
                     <p className="font-bold text-2xl py-2">EMAIL</p>
                     <p>Emergence2023@gmail.com</p>
                     <div className="flex w-full justify-end gap-4 max-[950px]:justify-center">

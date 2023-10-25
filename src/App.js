72f8fe263860413e1b08deee1fd144f5402ac0b3
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2023-10-25 23:32:52 +0530

    Updated Loader

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useState , useEffect } from "react";
 import HashLoader from "react-spinners/HashLoader";
 import Typed from "react-typed";
 
+
 import AOS from 'aos';
 import 'aos/dist/aos.css'
 
@@ -18,34 +19,33 @@ function App() {
     setLoading(true)
     setTimeout(()=> {
       setLoading(false)
-    },4500)
+    },10000)
   },[])
   return (
     <div className="App overflow-x-hidden">
       {
-        loading?
-        <div class="flex justify-center items-center h-screen w-screen bg-[#0c0d0d]">
-          <HashLoader
-            color="#E9441D"
-            loading
-            size={100}
-            speedMultiplier={1}
-          />
-          <Typed
-            className="md:text-2xl text-white sm:text-sm max-[500px]:text-base text-xl font-bold md:pl-4 pl-2"
-            strings={["EMERGENCE 3.0"]}
-            typeSpeed={110}
-          />
-        </div>
-      :
+        !loading?
       <div>
         <Appbar />
         <Home />
         <About />
         <Footer />
       </div>
+      :
+      <div class="flex justify-center items-center h-screen w-screen bg-[#0c0d0d]">
+          <HashLoader
+            color="#E9441D"
+            loading
+            size={100}
+            speedMultiplier={0.9}
+          />
+          <Typed
+            className="md:text-2xl text-white sm:text-sm max-[500px]:text-base text-xl font-bold md:pl-4 pl-2"
+            strings={["EMERGENCE 3.0"]}
+            typeSpeed={200}
+          />
+        </div>
       }
-      
     </div>
   );
 }
